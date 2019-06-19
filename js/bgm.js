9da6efe6a6b4d47f265b59b3277836904661ac2b
JoeWoo <48403634+Wuthemei@users.noreply.github.com>
2019-06-19 18:26:38 +0800

    Update bgm.js

--- js/bgm.js
+++ js/bgm.js
@@ -4,6 +4,15 @@ $(function(){
     $('body').css('height',document.documentElement.clientHeight -5);
 
     if (!Number.isInteger(arrMusicID[musicID])) return; // load failed, bye~
-
-    <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=' +arrMusicID[musicID]+ '&auto=1&height=66"></iframe>
+    var iframe = document.createElement('iframe');
+    iframe.id="bgm";
+    iframe.style = "position: absolute; top: 0px; left: 0px; border: 0px;";
+    iframe.src = '//music.163.com/outchain/player?type=2&id=' +arrMusicID[musicID]+ '&auto=1&height=32';
+    console.log(iframe.src)
+    iframe.frameborder="no";
+    iframe.marginwidth="0";
+    iframe.marginheight="0";
+    iframe.width=250;
+    iframe.height=52;
+    document.body.appendChild(iframe);
 });
