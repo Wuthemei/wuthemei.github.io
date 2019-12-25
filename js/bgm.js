0fbf91b8e09a147d4e990a148a6769f674e0d4aa
JoeWoo <48403634+Wuthemei@users.noreply.github.com>
2019-12-25 10:38:26 +0800

    Update bgm.js

--- js/bgm.js
+++ js/bgm.js
@@ -1,5 +1,5 @@
 $(function(){
-    arrMusicID = [4394416,1091088,27471609,26875199,27723552,26090100,26090100,26193921,501467597,19282045,306709,5299097,27471615,31062378,139443];  //musicID array
+    arrMusicID = [4394416,1091088,26875199,27723552,26090100,26193921,306709,5299097,31062378,139443,29984086,155924,432430716];  //musicID array
     musicID = Math.floor(Math.random()*(arrMusicID.length)) //get a ran num as index
     $('body').css('height',document.documentElement.clientHeight -5);
 
